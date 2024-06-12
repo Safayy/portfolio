7cdad4c35487eb5f8764a8648fd1bf80b056b09d
Safa Yousif <safa.yousif@outlook.com>
2024-06-12 18:24:08 +0800

    Add homepage

---
+++ src/components/Home.jsx
@@ -0,0 +1,34 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import '../styles/Home.css'
+
+const Home = () => {
+    const skills = ['Git', 'ReactJS', 'Android Studio', 'Java', 'PHP', 'HTML', 'CSS', 'Python', 'JavaScript', 'Docker', 'JUnit'];
+
+    return ( 
+        <div className='padding-30'>
+            <div id="introduction">
+                <p>Hello, I am</p>
+                <h1>Safa Yousif</h1>
+                <div className="socials">
+                    <FontAwesomeIcon icon={faGithub} className="icon" />
+                    <FontAwesomeIcon icon={faLinkedin} className="icon"/>
+                    <FontAwesomeIcon icon={faEnvelope} className="icon"/>
+                </div>
+                <h3>Fresh Graduate & Software Developer</h3>
+                <p>As a final year student deeply  passionate about Full Stack Development, my focus lies in mastering  React.js and Node.js. I thrive on exploring diverse technologies and  bringing innovative projects to life. I take.</p>
+            </div>
+            <div id="skills">
+                <span>What I Do</span>
+                <ul>
+                {skills.map((skill) =>
+                    <li className='tag' key={skill}>{skill}</li>
+                )}
+                </ul>
+            </div>
+        </div>
+     );
+}
+ 
+export default Home;

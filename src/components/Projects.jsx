2061cad746b3d4eac9cad39d3719281d696a1a0a
Safa Yousif <safa.yousif@outlook.com>
2024-06-20 19:32:36 +0800

    Add Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,27 @@
+import '../styles/Projects.css'
+import projects from '../assets/projects.json'
+
 const Projects = () => {
+    // console.log(projects)
     return ( 
-        <p>Projects</p>
+        <div className="padding-30">
+            <h1>Projects</h1>
+            <div className="cards-container">
+                {projects.map((project) => 
+                    <div key={project.id} className="card card-project">
+                        <h2>{project.name}</h2>
+                        <p>{project.description}</p>
+                        <span>{project.tags}</span>
+                        <button className='button background-blue' href={project.live_demo}>Live Demo</button>
+                        <button className='button' href={project.source_code}>Source Code</button>
+                        <video controls poster='image' preload='true'>
+                            <source src={'/linkedinbannermaker.mp4'} type="video/mp4"/>
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                )}
+            </div>
+        </div>
      );
 }
  

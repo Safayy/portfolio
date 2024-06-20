66a25c898d01b74d3228744dab6edabac8902e62
Safa Yousif <safa.yousif@outlook.com>
2024-06-20 19:32:51 +0800

    Add Blogs

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,6 +1,25 @@
+import '../styles/Blog.css'
+import blogs from '../assets/blogs.json'
+
 const Blog = () => {
     return ( 
-        <p>Blog</p>
+        <div className='padding-30'>
+            <h1>Blog</h1>
+            <div className='blogs-container'>
+                {blogs.length != 0 ?
+                blogs.map((blog) => 
+                <div key={blog.id} className='card blog-card'>
+                    <div>
+                        <span>{blog.date}</span>
+                        <h2>{blog.name}</h2>
+                        <p>{blog.body.substring(0, 100) + "..."}</p>
+                    </div>
+                    <img src='https://images.pexels.com/photos/736230/pexels-photo-736230.jpeg?cs=srgb&dl=pexels-jonaskakaroto-736230.jpg&fm=jpg'/>
+                </div>
+                ) : <p>No blog entires yet.</p>
+                }
+            </div>
+        </div>
      );
 }
  

121ee7daba513cc701e5764d538fd45ee2961efe
Safa Yousif <safa.yousif@outlook.com>
2024-06-24 15:33:33 +0800

    Add tags delimeter

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,16 +2,16 @@ import '../styles/Projects.css'
 import projects from '../assets/projects.json'
 
 const Projects = () => {
-    // console.log(projects)
+
     return ( 
         <div className="padding-30">
             <h1>Projects</h1>
             <div className="cards-container">
-                {projects.map((project) => 
+                {projects.map((project) =>
                     <div key={project.id} className="card card-project">
                         <h2>{project.name}</h2>
                         <p>{project.description}</p>
-                        <span>{project.tags}</span>
+                        <span>{project.tags.join(', ')}</span>
                         <button className='button background-blue' href={project.live_demo}>Live Demo</button>
                         <button className='button' href={project.source_code}>Source Code</button>
                         <video controls poster='image' preload='true'>

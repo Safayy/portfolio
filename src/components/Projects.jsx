bd9280f69f41cee5d7b10f5f6466fd43f49d5585
Safa Yousif <safa.yousif@outlook.com>
2024-06-25 10:40:00 +0800

    Fix project links

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,9 +12,9 @@ const Projects = () => {
                         <h2>{project.name}</h2>
                         <p>{project.description}</p>
                         <span>{project.tags.join(', ')}</span>
-                        { project.release_link && <button className='button background-blue' target="_blank" href={project.release_link}>Release</button> }
-                        { project.live_demo && <button className='button background-blue' target="_blank" href={project.live_demo}>Live Demo</button> }
-                        { project.source_code && <button className='button' target="_blank" href={project.source_code}>Source Code</button> }
+                        { project.release_link && <button className='button background-blue' onClick={() => window.open(project.release_link, '_blank')}>Release</button> }
+                        { project.live_demo && <button className='button background-blue' onClick={() => window.open(project.live_demo, '_blank')}>Live Demo</button> }
+                        { project.source_code && <button className='button' onClick={() => window.open(project.source_code, '_blank')}>Source Code</button> }
                         <video className='fixed-height' controls poster='image' preload='true'>
                             <source src={  '/' + project.demo_video } type="video/mp4"/>
                             Your browser does not support the video tag.
